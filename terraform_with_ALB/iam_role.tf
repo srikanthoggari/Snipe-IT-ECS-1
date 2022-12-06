0ed7761f11b39b503b96da6a4ccb88a0f4163481
Sai <Vgoud5959@gmail.com>
2022-12-06 21:43:18 +0530

    add

--- terraform_with_ALB/iam_role.tf
+++ terraform_with_ALB/iam_role.tf
@@ -11,15 +11,3 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
     }
   }
 }
-
-# ECS task execution role
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "myECcsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
-}
-
-# ECS task execution role policy attachment
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
