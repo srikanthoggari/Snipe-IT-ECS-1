3873330201247cdf3c220dfcde96ca4d6e506084
Sai <Vgoud5959@gmail.com>
2022-12-06 21:59:21 +0530

    asag

--- terraform_with_ALB/iam_role.tf
+++ terraform_with_ALB/iam_role.tf
@@ -20,5 +20,5 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # ECS task execution role policy attachment
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::213625440773:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
